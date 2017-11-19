b52b2e9683604568ee613f4e289a22339dc7bef8
Brendan Lucas <brendanwflucas@gmail.com>
2017-11-18 23:38:44 -0500

    Light fix: remove hardcodes in pushed code

--- QueryScript.py
+++ QueryScript.py
@@ -2,7 +2,7 @@ import json
 
 runStartup = False;
 
-
+CAPACITY = 80000
 DATABASEFILE = 'Store.json'
 gStorage = {}
 gItems = {}
@@ -26,10 +26,13 @@ def dict_maker(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
     return {"Name" : name, "volume" : volume, "ExpirationDate" : expirationDate, "PriceBTX" : priceBTX, "PriceATX" : PriceATX, "DateAdded" : dateAdded}
 
 
+
+
 if runStartup:
+    preVolume = 200
     #populate json file with some Items before we do add anything.
     PriceATX = 23* 1.23
-    Starting_List = {"Items":[{"Name" : "Apples", "ExpirationDate" : "2017-04-14", "PriceBTX" : "23", "PriceATX" : PriceATX, "DateAdded" : "2017-11-18", "Volume" : "14"}], "Storage" : { "Size" : 80000, "Remaining" : 20000} }
+    Starting_List = {"Items":[{"Name" : "Apples", "ExpirationDate" : "2017-04-14", "PriceBTX" : preVolume, "PriceATX" : PriceATX, "DateAdded" : "2017-11-18", "Volume" : "14"}], "Storage" : { "Size" : CAPACITY, "Remaining" : CAPACITY - preVolume} }
     print("Starting_List: ", repr(Starting_List))
     print("JSON: ", json.dumps(Starting_List))
 
