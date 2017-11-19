38fb92b03f3f51ffc7df785ec4af0db88e3ea724
juliassj <julia.stjean@hotmail.com>
2017-11-19 01:00:52 -0500

    Update QUERIES.py

--- QUERIES.py
+++ QUERIES.py
@@ -1,5 +1,5 @@
 import json
-
+import datetime
 def itemsWithName(name, filepath):
     file = open(filepath, 'r')
     JSON =file.read()
@@ -26,4 +26,27 @@ def storageStatus(filepath):
     capacity =(d["Storage"]["Size"]-d["Storage"]["Remaining"])/d["Storage"]["Size"]
     print ("Container is " + str(int(capacity*100))+ "% full!")
 
+
+def daysToExpiry(filepath, name):
+    file = open(filepath, 'r')
+    JSON = file.read()
+    file.close()
+    d = json.loads(JSON)
+    for item in d["Items"]:
+        if item["Name"] == name:
+            daystillexp=item["ExpirationDate"]
+            year = int((item["DateAdded"])[0:4])
+            month=int((item["DateAdded"])[5:7])
+            day = int((item["DateAdded"])[8:10])
+            day+=daystillexp
+            if (day>30):
+                month += day/31
+                day=day%31
+
+                if (month >12):
+                    year+=month/13
+                    month=month%13
+            daysleft =(year-datetime.datetime.now().year)*365 +(month-datetime.datetime.now().month)*12 +(day-datetime.datetime.now().day)
+
+            return daysleft
 
