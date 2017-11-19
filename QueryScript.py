c745c452a0e53e278f63d275623682fb917c560e
Brendan Lucas <brendanwflucas@gmail.com>
2017-11-18 23:43:10 -0500

    FORGOT return value

--- QueryScript.py
+++ QueryScript.py
@@ -7,6 +7,7 @@ DATABASEFILE = 'Store.json'
 gStorage = {}
 gItems = {}
 
+"""QUERY: AddItem"""
 def AddItem(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
 
     dict = dict_maker(name, volume, priceBTX, priceATX, expirationDate, dateAdded)
@@ -19,12 +20,42 @@ def AddItem(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
     gItems.append(dict)
     gStorage["Remaining"] -= volume
 
-    jsonData = {"Items": gItems, "Storage" : gStorage}
+    jsonData = {"Items": gItems, "Storage": gStorage}
+    with open(DATABASEFILE, 'w') as jsonFile:
+        json.dump(jsonData, jsonFile)
+
 
 
 def dict_maker(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
     return {"Name" : name, "volume" : volume, "ExpirationDate" : expirationDate, "PriceBTX" : priceBTX, "PriceATX" : PriceATX, "DateAdded" : dateAdded}
 
+"""QUERY: Items With Name"""
+def itemsWithName(name, filepath):
+    file = open(filepath, 'r')
+    JSON =file.read()
+    file.close()
+    d=json.loads(JSON)
+    for item in d["Items"]:
+        if item["Name"] == name:
+            print (item)
+
+""""QUERY: All Items"""
+def allItems(filepath):
+    file = open(filepath, 'r')
+    JSON = file.read()
+    file.close()
+    d = json.loads(JSON)
+    for item in d["Items"]:
+       print(item)
+
+""" QUERY: Storage Status """
+def storageStatus(filepath):
+    file = open(filepath, 'r')
+    JSON = file.read()
+    file.close()
+    d = json.loads(JSON)
+    capacity =(d["Storage"]["Size"]-d["Storage"]["Remaining"])/d["Storage"]["Size"]
+    print ("Container is " + str(int(capacity*100))+ "% full!")
 
 
 
