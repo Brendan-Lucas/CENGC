fbe6c2b7178379ebf31112205cb67038e3a80dbe
matt1light <matt1light@gmail.com>
2017-11-19 00:53:41 -0500

    incorporate validation into AddItem

--- QueryScript.py
+++ QueryScript.py
@@ -1,5 +1,6 @@
 import json
 import uuid
+import BuildItem
 
 runStartup = False;
 
@@ -9,7 +10,17 @@ gStorage = {}
 gItems = {}
 
 """QUERY: AddItem"""
-def AddItem(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
+
+def AddItem(name, volume, price, daysToExpiration):
+    item = BuildItem.build_item(name, volume, price, daysToExpiration)
+    if not item["errors"]:
+        CreateItem(item["name"], item["volume"], item["price_before_tax"], item["price_after_tax"], item["expiration_date"], item["date_added"])
+    else:
+        print "Add item was not completed"
+        for error in item["errors"]:
+            print error
+
+def CreateItem(name, volume, priceBTX, priceATX, expirationDate, dateAdded):
 
     generatedID = uuid.UUID().int()
     dict = dict_maker(name, volume, priceBTX, priceATX, expirationDate, dateAdded, generatedID)
@@ -74,7 +85,4 @@ if runStartup:
 
     with open(DATABASEFILE, 'w') as file:
         json.dump(Starting_List, file)
-
-    
-
 
